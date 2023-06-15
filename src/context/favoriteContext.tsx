3e597740e99660044885fff16885904afefb5090
cristianmeelo <cristianmeelo@gmail.com>
2023-06-15 09:36:16 -0300

    :sparkles: feat: create favorite context

--- src/context/favoriteContext.tsx
+++ src/context/favoriteContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 interface FavoriteContextType {
   favorite: string[];
@@ -17,4 +17,22 @@ export const FavoriteProvider = ({ children }: { children: React.ReactNode }) =>
   };
 
   return <FavoriteContext.Provider value={context}>{children}</FavoriteContext.Provider>;
+};
+
+export const useFavoriteContext = () => {
+  const { favorite, setFavorite } = useContext(FavoriteContext);
+
+  const addFavorite = (newFavorite: any) => {
+    const repeatFavorite = favorite.some((item: any) => item.id === newFavorite.id);
+
+    let newList = [...favorite];
+    if (!repeatFavorite) {
+      newList.push(newFavorite);
+      return setFavorite(newList);
+    }
+
+    newList = favorite.filter((item: any) => item.id !== newFavorite.id);
+    return setFavorite(newList);
+  };
+  return { favorite, addFavorite };
 };
