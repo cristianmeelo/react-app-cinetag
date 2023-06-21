0d2f60238a3cf044d692974d7fc4b8a8ac8b870d
cristianmeelo <cristianmeelo@gmail.com>
2023-06-21 17:55:28 -0300

    :recycle: refactor: changed name to solve import problems

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
-export { HeaderLink } from "./HeaderLink";
-export { Footer } from "./Footer";
-export { Header } from "./Header";
+export { HeaderLink } from "./AnotherHeaderLink";
+export { Footer } from "./AnotherFooter";
+export { Header } from "./AnotherHeader";
 export { Title } from "./Title";
 export { Banner } from "./Banner";
 export { Container } from "./Container";
