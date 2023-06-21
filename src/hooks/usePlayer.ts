83daf48a7da362c372b53ce1e773048896f733fa
cristianmeelo <cristianmeelo@gmail.com>
2023-06-21 08:55:33 -0300

    :tada: project finalized

--- src/hooks/usePlayer.ts
+++ src/hooks/usePlayer.ts
@@ -1,11 +1,15 @@
 import { useParams } from "react-router-dom";
-import { useFetchMovies } from "./useFetchMovies";
+import { useEffect, useState } from "react";
 
 export const usePlayer = () => {
-  const { movies } = useFetchMovies();
-
+  const [movie, setMovie] = useState<IMovies>();
   const params = useParams();
-  const movie = movies.find((movie) => movie.id === Number(params.id));
+
+  useEffect(() => {
+    fetch(`https://my-json-server.typicode.com/cristianmeelo/cinetag-api/movies?id=${params?.id}`)
+      .then((resp) => resp.json())
+      .then((data) => setMovie(data[0]));
+  }, []);
 
   if (!movie) {
     return { notFound: true };
--- src/pages/player/index.tsx
+++ src/pages/player/index.tsx
@@ -4,11 +4,11 @@ import styles from "./index.module.css";
 
 export const Player = () => {
   const { movie } = usePlayer();
-
+  console.log("%cindex.tsx line:7 mooie", "color: #26bfa5;", movie);
   return (
     <>
       <Banner image={"player"} />
-      <Title>PLayer</Title>
+      <Title>Player</Title>
       <section className={styles.container}>
         <iframe width="100%" height="100%" src={movie?.link} title={movie?.title} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
       </section>
