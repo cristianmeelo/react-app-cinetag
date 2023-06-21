c817eabf6eee44712b224a8b9ba6073e8f23afd6
cristianmeelo <cristianmeelo@gmail.com>
2023-06-21 18:06:03 -0300

    :sparkles: feat: update Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import styles from "./index.module.css";
 export const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <h2>Desenvolvido por Alura.</h2>
+      <h2>Desenvolvido por Alura / Cristian Melo.</h2>
     </footer>
   );
 };
