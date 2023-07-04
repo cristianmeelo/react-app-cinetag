e54f87cf5900d4f61ab9011e1120ebf635e08139
cristianmeelo <cristianmeelo@gmail.com>
2023-07-04 14:17:24 -0300

    :bug: fix: fixed expected children in Title Component

--- src/pages/player/index.tsx
+++ src/pages/player/index.tsx
@@ -4,11 +4,13 @@ import styles from "./index.module.css";
 
 export const Player = () => {
   const { movie } = usePlayer();
-  console.log("%cindex.tsx line:7 mooie", "color: #26bfa5;", movie);
+
   return (
     <>
       <Banner image={"player"} />
-      <Title>Player</Title>
+      <Title>
+        <h1>Player</h1>
+      </Title>
       <section className={styles.container}>
         <iframe width="100%" height="100%" src={movie?.link} title={movie?.title} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
       </section>
